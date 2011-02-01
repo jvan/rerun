fc59f4959d0eaa69fb42e64fb896eeb91875786b
Braden <contact@dabrado.net>
2011-01-31 18:16:04 -0800

    First crack at some unit testing.

---
+++ test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Run some tests on rerun.
+#
+
+# Helper to check output from macros
+check() {
+   local cmd=$1; shift
+   echo -n '***' $cmd 'result: ' >&2
+   while read; do
+      (($#==0)) && { echo "failed (extra output)." >&2; return 1; } 
+      (($1==$REPLY)) || { echo "failed ($1 != $REPLY)." >&2; return 1; }
+      shift
+   done <<<"$($cmd)"
+   (($#==0)) || { echo "failed (more output expected)." >&2; return 1; }
+   echo "okay." >&2
+}
+
+# Establish some history
+set -o history
+echo 1
+echo 2
+echo 3
+echo 4
+echo 5
+set +o history
+
+# Bring in rerun
+source rerun.sh
+
+# Creation from list
+t=create_from_list
+rerun create $t 1 4 3 2 5
+check $t 1 4 3 2 5
+
+# Creation from range
+t=create_from_range
+rerun create $t 1-5
+check $t 1 2 3 4 5
+
+# Creation from start:count
+t=create_from_start_and_count
+rerun create $t 1:5
+check $t 1 2 3 4 5
+
+# Creation from include spec
+t=create_from_include_spec
+rerun create $t 1.--.
+check $t 1 2 5
